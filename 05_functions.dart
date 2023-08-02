2b580f2df3773f66347863d4fa5b07156106024c
Andres Felipe Pizo Luligo <pizoluligoa@gmail.com>
2023-08-02 15:04:25 -0500

    Se modifica archivo de function

--- 05_functions.dart
+++ 05_functions.dart
@@ -7,6 +7,9 @@ void main () {
   print('Three numbers: ${ addThreeNumbers(5,1,4) }');
   
   print('Add numbers optionals: ${ addTwoNumbersOptional(4, 8) }');
+  
+  print( greetPerson( name: 'Andrés ñ' ) );
+  
 }
 
 String greenEveryoneFunctionNormal() {
@@ -22,5 +25,10 @@ int addTwoNumbers( int a, int b ) {
 int addThreeNumbers(int a, int b, int c) => a + b + c;
 
 int addTwoNumbersOptional(int a, [ int b = 0 ]) {
+  // b ??= 0; cuando se quiere que sea optional
   return a + b;
+}
+
+String greetPerson({ required String name, String message = 'Hola hpta,' }) {
+  return '$message $name';
 }
